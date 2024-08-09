09d851946235d09630fe6ba1386c9526b4133591
Prachi Sharma <97446551+prachi070@users.noreply.github.com>
2024-08-09 17:02:32 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import {BrowserRouter,Route, Routes} from "react-router-dom"
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Header from './components/Header'
 const App = () => {
   return (
-    <div>
-      <h1>prachi sharma</h1>
-    </div>
+   <BrowserRouter>
+   <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/profile' element={<Profile/>} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
